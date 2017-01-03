6d8b4b60660bf31339538cb008b52adabf008af1
wlqsltz <wlqsltz@163.com>
2017-01-03 09:42:44 +0800

    修复v100第四轮bug

--- static/js/app/biz/order.js
+++ static/js/app/biz/order.js
@@ -1,9 +1,9 @@
-$(function () {
+$(function() {
     var columns = [{
         field: '',
         title: '',
         checkbox: true
-    },{
+    }, {
         field: 'code',
         title: '订单号'
     }, {
@@ -12,34 +12,33 @@ $(function () {
     }, {
         field: 'realName',
         title: '真实姓名',
-        value:sessionStorage.getItem('userName')
-    },{
+        value: sessionStorage.getItem('userName')
+    }, {
         field: 'idNo',
-        title: '身份证号',
-        class: "tableexport-msonumberformat1"
-    },{
+        title: '身份证号'
+    }, {
         field: 'mobile',
         title: '手机号',
         search: true
-    },{
+    }, {
         field: 'totalNum',
         title: '人次'
-    },{
-    	field:'totalAmount',
+    }, {
+        field: 'totalAmount',
         title: '订单总额',
         formatter: moneyFormat
-    },{
+    }, {
         field: 'hotelName',
         title: '名宿名称'
-    },{
+    }, {
         field: 'status',
         title: '状态',
         formatter: Dict.getNameForList('order_status'),
         search: true,
         type: 'select',
         key: 'order_status'
-       
-    },{
+
+    }, {
         field: 'payDatetime',
         title: '支付时间',
         formatter: dateTimeFormat,
@@ -48,20 +47,20 @@ $(function () {
         field1: 'payBeginDatetime',
         field2: 'payEndDatetime',
         search: true
-    },{
-    	title: '备注',
-		field: 'remark',
-		maxlength: 250
-		
-	}];
+    }, {
+        title: '备注',
+        field: 'remark',
+        maxlength: 250
+
+    }];
 
     buildList({
         router: 'order',
         columns: columns,
         pageCode: '616212'
     });
-      
-    $('#refundBtn').click(function () {
+
+    $('#refundBtn').click(function() {
         var selRecords = $('#tableList').bootstrapTable('getSelections');
         if (selRecords.length <= 0) {
             toastr.info("请选择记录");
@@ -69,7 +68,7 @@ $(function () {
         }
         window.location.href = "order_addedit.html?code=" + selRecords[0].code + "&name=5";
     });
-    $('#contactBtn').click(function () {
+    $('#contactBtn').click(function() {
         var selRecords = $('#tableList').bootstrapTable('getSelections');
         if (selRecords.length <= 0) {
             toastr.info("请选择记录");
@@ -77,7 +76,7 @@ $(function () {
         }
         window.location.href = "order_addedit.html?code=" + selRecords[0].code + "&name=2";
     });
-    $('#visitedBtn').click(function () {
+    $('#visitedBtn').click(function() {
         var selRecords = $('#tableList').bootstrapTable('getSelections');
         if (selRecords.length <= 0) {
             toastr.info("请选择记录");
@@ -85,6 +84,6 @@ $(function () {
         }
         window.location.href = "order_addedit.html?code=" + selRecords[0].code + "&name=7";
     });
-    
-    
+
+
 });
--- static/js/app/biz/wonderful.js
+++ static/js/app/biz/wonderful.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     var columns = [{
         field: '',
         title: '',
@@ -21,79 +21,78 @@ $(function () {
         search: true,
         type: 'select',
         key: 'location_status'
-    },{
+    }, {
         field: 'publisher',
         title: '最近更新人',
-        //valueName: "updater",
-        formatter: function(value, row, index){
-        	return row['updater'] || value;
+        formatter: function(value, row, index) {
+            return row['updater'] || value;
         }
 
-    },{
-    	field:'publishDatetime',
+    }, {
+        field: 'publishDatetime',
         title: '最近更新时间',
-        formatter: function(value, row, index){
-        	return row['updateDatetime'] ? dateTimeFormat(row['updateDatetime']) : dateTimeFormat(value);
+        formatter: function(value, row, index) {
+            return row['updateDatetime'] ? dateTimeFormat(row['updateDatetime']) : dateTimeFormat(value);
         }
 
-    },{
-		title: '备注',
-		field: 'remark',
-		maxlength: 250
-	}];
+    }, {
+        title: '备注',
+        field: 'remark',
+        maxlength: 250
+    }];
 
     buildList({
         router: 'wonderful',
         columns: columns,
         pageCode: '616190',
-        searchParams:{type:"4"}
-//        addCode:''
+        searchParams: { type: "4" }
+        //        addCode:''
     });
 
-    
-	$('#frameBtn').click(function() {
-		var selRecords = $('#tableList').bootstrapTable('getSelections');
-		if(selRecords.length <= 0){
-			toastr.info("请选择记录");
-			return;
-		}
-		var msg = "确认上架该活动？";
-		if(selRecords[0].status == 1){
-			msg = "确认下架该活动？";
-		}
 
-		confirm(msg).then(function() {
-			reqApi({
-				code: '616183',
-				json: {"code": selRecords[0].code}
-			}).then(function() {
-				toastr.info("操作成功");
-				$('#tableList').bootstrapTable('refresh', { url: $('#tableList').bootstrapTable('getOptions').url });
-			});
-		});
-	});
-	
-	$('#topBtn').click(function() {
-		var selRecords = $('#tableList').bootstrapTable('getSelections');
-		if(selRecords.length <= 0){
-			toastr.info("请选择记录");
-			return;
-		}
-		if(selRecords[0].location == 1){
-			toastr.warning("该活动已置顶");
-			return;
-		}
-		
-		confirm("确认置顶该活动？").then(function() {
-			reqApi({
-				code: '616184',
-				json: {"code": selRecords[0].code, location: 1}
-			}).then(function() {
-				toastr.info("操作成功");
-				$('#tableList').bootstrapTable('refresh', { url: $('#tableList').bootstrapTable('getOptions').url });
-			});
-		});
-	});
+    $('#frameBtn').click(function() {
+        var selRecords = $('#tableList').bootstrapTable('getSelections');
+        if (selRecords.length <= 0) {
+            toastr.info("请选择记录");
+            return;
+        }
+        var msg = "确认上架该活动？";
+        if (selRecords[0].status == 1) {
+            msg = "确认下架该活动？";
+        }
 
-	
+        confirm(msg).then(function() {
+            reqApi({
+                code: '616183',
+                json: { "code": selRecords[0].code }
+            }).then(function() {
+                toastr.info("操作成功");
+                $('#tableList').bootstrapTable('refresh', { url: $('#tableList').bootstrapTable('getOptions').url });
+            });
+        });
+    });
+
+    $('#topBtn').click(function() {
+        var selRecords = $('#tableList').bootstrapTable('getSelections');
+        if (selRecords.length <= 0) {
+            toastr.info("请选择记录");
+            return;
+        }
+        if (selRecords[0].location == 1) {
+            toastr.warning("该活动已置顶");
+            return;
+        }
+
+        confirm("确认置顶该活动？").then(function() {
+            reqApi({
+                code: '616184',
+                json: { "code": selRecords[0].code, location: 1 }
+            }).then(function() {
+                toastr.info("操作成功");
+                $('#tableList').bootstrapTable('refresh', { url: $('#tableList').bootstrapTable('getOptions').url });
+            });
+        });
+    });
+
+
 });
